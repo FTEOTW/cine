812e2e401a872aad492b49b7d1fe100c30d1848a
FTEOTW <avila344@gmail.com>
2020-11-01 19:08:10 -0300

    add separated class to allow load combo from different places

---
+++ Cine/utils/LoadInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Cine.database;
+
+namespace Cine.utils
+{
+    class LoadInfo
+    {
+        public void CargarCombo(ComboBox combo, String table)
+        {
+            DataTable dt = new DataTable();
+            ConsultaDB consulta = new ConsultaDB();
+            consulta.ejecutarConsulta("exec " + table, 1);
+            dt = consulta.pGetTable;
+            combo.DataSource = dt;
+            combo.ValueMember = dt.Columns[0].ColumnName;
+            try //esto es para cuando llenamos combo con tablas que no tienen id, llamando a sp
+            {
+                combo.DisplayMember = dt.Columns[1].ColumnName;
+            }
+            catch (IndexOutOfRangeException)
+            {
+            }
+            
+        }
+    }
+}
